a34fb9e942a4af14e7821e5c3251b2e550cddf87
konnerb <konner.bouchard@hotmail.com>
2020-09-19 11:54:44 -0400

    Added Option To Create Folders Within Current_Directory

--- app.py
+++ app.py
@@ -18,34 +18,42 @@ current_dir = input("Folder Pathway: ")
 if len(current_dir) == 0:
   print("Please Enter Folder Pathway")
 else:
-  confirm_input = input("Are Your Sure? (y/n) ")
+  confirm_input = input(f'Confirm Folder Pathway {current_dir} (y/n) ')
   if confirm_input == 'y':
-    print("Sorting Files...")
-    for f in os.listdir(current_dir):
-      filename, file_ext = os.path.splitext(f)
-      print(f)
-      try:
-        if not file_ext:
-          pass
-        elif file_ext in ('.png', '.jpg', 'jpeg'):
-          shutil.move(
-            os.path.join(current_dir, f'{filename}{file_ext}'),
-            os.path.join(current_dir, 'Images', f'{filename}{file_ext}'))
-        elif file_ext in ('.mp3', '.aiff'):
-          shutil.move(
-            os.path.join(current_dir, f'{filename}{file_ext}'),
-            os.path.join(current_dir, 'Music', f'{filename}{file_ext}'))
-        elif file_ext in ('.pdf'):
-          shutil.move(
-            os.path.join(current_dir, f'{filename}{file_ext}'),
-            os.path.join(current_dir, 'Documents', f'{filename}{file_ext}'))
-        else:
-          shutil.move(
-            os.path.join(current_dir, f'{filename}{file_ext}'),
-            os.path.join(current_dir, 'Other Stuff', f'{filename}{file_ext}'))
+    folders = [item for item in input("Enter Folders seperated with coma : ").split(",")] 
+    confirm_folders = input(f'Confirm Folders Created "{folders}" (y/n) ')
+    if confirm_folders == 'y':
+      gh = folders
+      for folder in gh:
+        os.mkdir(os.path.join(current_dir,str(folder)))
+        print(f'Created Folder...{folder}')
+        for f in os.listdir(current_dir):
+          filename, file_ext = os.path.splitext(f)
+          print(f)
+          try:
+            if not file_ext:
+              pass
+            elif file_ext in ('.png', '.jpg', 'jpeg'):
+              shutil.move(
+                os.path.join(current_dir, f'{filename}{file_ext}'),
+                os.path.join(current_dir, 'Images', f'{filename}{file_ext}'))
+            elif file_ext in ('.mp3', '.aiff'):
+              shutil.move(
+                os.path.join(current_dir, f'{filename}{file_ext}'),
+                os.path.join(current_dir, 'Music', f'{filename}{file_ext}'))
+            elif file_ext in ('.pdf'):
+              shutil.move(
+                os.path.join(current_dir, f'{filename}{file_ext}'),
+                os.path.join(current_dir, 'Documents', f'{filename}{file_ext}'))
+            else:
+              shutil.move(
+                os.path.join(current_dir, f'{filename}{file_ext}'),
+                os.path.join(current_dir, 'Other Stuff', f'{filename}{file_ext}'))
 
-      except (FileNotFoundError, PermissionError):
-        pass
+          except (FileNotFoundError, PermissionError):
+            pass
+      else:
+        print("Please Enter Required Folder")
   else:
     print("Please Enter Correct File Pathway")
 
