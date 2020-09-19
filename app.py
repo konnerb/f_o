752ade76114a6dc28d5bc98a844239c81f3c9e05
konnerb <konner.bouchard@hotmail.com>
2020-09-19 12:24:37 -0400

    Created sort_files And create_files Functions

--- app.py
+++ app.py
@@ -7,11 +7,50 @@ import shutil
 # # os.chdir('')
 
 
+
 print("""
 **** Running...f_o ****
 
 Please Enter Folder Pathway. Ex Users/Documents/sort_folder
 """)
+
+def create_files(cur_dir):
+  folders = [item for item in input("Enter Folders seperated with coma : ").split(",")] 
+  confirm_folders = input(f'Confirm Folders Created "{folders}" (y/n) ')
+  if confirm_folders == 'y':
+    gh = folders
+    for folder in gh:
+      os.mkdir(os.path.join(cur_dir,str(folder)))
+      print(f'Created Folder...{folder}')
+  else:
+    print("Please Enter Required Folder")
+
+def sort_files(cur_dir):
+  for f in os.listdir(cur_dir):
+    filename, file_ext = os.path.splitext(f)
+    print(f)
+    try:
+      if not file_ext:
+        pass
+      elif file_ext in ('.png', '.jpg', 'jpeg'):
+        shutil.move(
+          os.path.join(current_dir, f'{filename}{file_ext}'),
+          os.path.join(current_dir, 'Images', f'{filename}{file_ext}'))
+      elif file_ext in ('.mp3', '.aiff'):
+        shutil.move(
+          os.path.join(current_dir, f'{filename}{file_ext}'),
+          os.path.join(current_dir, 'Music', f'{filename}{file_ext}'))
+      elif file_ext in ('.pdf'):
+        shutil.move(
+          os.path.join(current_dir, f'{filename}{file_ext}'),
+          os.path.join(current_dir, 'Documents', f'{filename}{file_ext}'))
+      else:
+        shutil.move(
+          os.path.join(current_dir, f'{filename}{file_ext}'),
+          os.path.join(current_dir, 'Other Stuff', f'{filename}{file_ext}'))
+
+    except (FileNotFoundError, PermissionError):
+      pass
 
 current_dir = input("Folder Pathway: ")
 
@@ -20,40 +59,8 @@ if len(current_dir) == 0:
 else:
   confirm_input = input(f'Confirm Folder Pathway {current_dir} (y/n) ')
   if confirm_input == 'y':
-    folders = [item for item in input("Enter Folders seperated with coma : ").split(",")] 
-    confirm_folders = input(f'Confirm Folders Created "{folders}" (y/n) ')
-    if confirm_folders == 'y':
-      gh = folders
-      for folder in gh:
-        os.mkdir(os.path.join(current_dir,str(folder)))
-        print(f'Created Folder...{folder}')
-        for f in os.listdir(current_dir):
-          filename, file_ext = os.path.splitext(f)
-          print(f)
-          try:
-            if not file_ext:
-              pass
-            elif file_ext in ('.png', '.jpg', 'jpeg'):
-              shutil.move(
-                os.path.join(current_dir, f'{filename}{file_ext}'),
-                os.path.join(current_dir, 'Images', f'{filename}{file_ext}'))
-            elif file_ext in ('.mp3', '.aiff'):
-              shutil.move(
-                os.path.join(current_dir, f'{filename}{file_ext}'),
-                os.path.join(current_dir, 'Music', f'{filename}{file_ext}'))
-            elif file_ext in ('.pdf'):
-              shutil.move(
-                os.path.join(current_dir, f'{filename}{file_ext}'),
-                os.path.join(current_dir, 'Documents', f'{filename}{file_ext}'))
-            else:
-              shutil.move(
-                os.path.join(current_dir, f'{filename}{file_ext}'),
-                os.path.join(current_dir, 'Other Stuff', f'{filename}{file_ext}'))
-
-          except (FileNotFoundError, PermissionError):
-            pass
-      else:
-        print("Please Enter Required Folder")
+    create_files(current_dir)
+    sort_files(current_dir)
   else:
     print("Please Enter Correct File Pathway")
 
