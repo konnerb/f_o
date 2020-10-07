9b7e5b673074012d80ccd12a5326ff863ed3f7e4
konnerb <konner.bouchard@hotmail.com>
2020-10-07 17:42:11 -0400

    Edited README.md

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ A simple and dynamic file organizer created in Python.
 
 ### How it works? 
 
-After entering your desired directory for sorting, f_o will verify if the directory exists and if any files exist. Then, with the given configuration, f_o will check which folders need to be created for the sorted files. After confirming the folders created, f_o will begin sorting all the files in the given directory according to the file extensions. 
+After entering your desired directory for sorting, f_o will verify if the directory exists and if any files exist. Then, with the given configuration, f_o will check which folders need to be created for the sorted files. After confirming the folders created, f_o will begin sorting or deleting all the files in the given directory according to the given keywords or file extensions. 
 
 ### Editing the config
 
-In the config.py file, f_o comes with stock presets for creating new folders and sorting files. You can customize the config file as long as there is more than one key, as in "Images" and more than one file extension value ['.png', ...]. Any files that are not in the config will be automatically sorted into an 'Others' folder.
-
-### Whats next?
-
-In the near future, there will be additional support for sorting files based on given key-words. 
+In the config.py file, f_o comes with stock presets for creating new folders and sorting files. You can customize the config file as long as there is more than one key, as in 'Music', and more than one value ['.mp4', '...'] or ['House', '...'] is in the config dictionary below. Key values are case-sensitive and all files that are not found in the config will be automatically sorted into an 'Others' folder.
 
 ## How to run locally
 
--- config.py
+++ config.py
@@ -1,43 +1,47 @@
 '''
-For f_o to work, ensure more than one key, as in 'Music', and more than one value ['.mp4'] or ['Hip-Hop'] is 
-in the config dictionary below. Any files that are not in the config will be automatically sorted 
-into an 'Others' folder.
+For f_o to work, ensure more than one key, as in 'Music', and more than one value ['.mp4', '...'] or 
+['Hip-Hop', ['...'] is in the config dictionary below. Values are case censitive and all files that 
+are not found in the config will be automatically sorted into an 'Others' folder.
 
 You can enable or disable keys/values by commenting them out with # for single line or ''' ''' for multiline.
 
 **** USE THE DELETE KEY WITH CAUTION. ANY VALUE ENTERED WILL DELETE YOUR FILES FOREVER ****
+        >>Consider creating a 'Trash' Folder for files you wish to delete. 
+          Then review and discard of the files afterwords.
   
   Examples:
 
     Delete by keywords or extentions.
     'DELETE': [House', 'Hip-Hop', 'Pop', '.png', '.mp3', '.pdf'],
 
-    Sort files by generic file extentions:
+    Or sort files by generic file extentions:
       >> 'Images': ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.HEIC'],
       >> 'Documents': ['.pdf', '.doc', '.docx', '.xls', '.odt', '.ps', '.wpd', '.html'],
       >> 'Music': ['.mp3', '.aiff', '.wav', '.acc', '.m4p', '.aa'],
       >> 'Movies': ['.mp4', '.mov', '.flv', '.m4v', '.3gp', '.3gp', 'MTS', '.M2TS', '.TS']
+      >> 'Trash': ['.mp4', '.png', '.pdf']
     
     Or sort files by keywords e.g. 'value'
       >> 'Genre': ['House', 'Hip-Hop', 'Pop'],
       >> 'Cuisine': ['French', 'Jamican', 'Mexican'],
+      >> 'Trash': ['random_stuff', 'random', 'stuff']
 
     Or delete files with keywords e.g. '.value'
-      >> 'DELETE': ['House', 'Hip-Hop', 'Pop'],
+      >> 'DELETE': ['House', 'Hip-Hop', '.png', '.pdf'],
 
 '''
 
 # Edit config here:
 config: dict = {
   
-    # Delete by keywords or extentions.
+    # Delete by keywords or extentions here:  e.g. 'value' or '.value'
     'DELETE': ['None'], # <<< *** CAUTION: ANY FILE WITH VALUES ENTERED HERE WILL BE PERMANENTLY DELETED
 
-    # Sort by keywords e.g. 'value'
+    # Sort by keywords here:  e.g. 'value'
     'Genre': ['House', 'Hip-Hop', 'Pop'],
     'Cuisine': ['French', 'Jamican', 'Mexican'],
 
-    # Sort by file extention e.g. '.value'
+    # Sort by file extention here:  e.g. '.value'
     'Images': ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.HEIC'],
     'Documents': ['.pdf', '.doc', '.docx', '.xls', '.odt', '.ps', '.wpd', '.html'],
     'Music': ['.mp3', '.aiff', '.wav', '.acc', '.m4p', '.aa'],
